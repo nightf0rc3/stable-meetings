983fc0cf20ed8e0bca171f9fbfe5a8006c74c4bc
Janim <84266727+janim1807@users.noreply.github.com>
2023-10-06 22:44:11 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/demo')
 def greet():
     return send_from_directory('./dist', 'indexDemo.html')
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    return "Test successful"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80, host='0.0.0.0')
