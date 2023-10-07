1ebb61ee2e99801f786c7133d2e41d708485617d
Karl <68251820+karltlf@users.noreply.github.com>
2023-10-07 02:53:40 +0200

    update files

--- dist/connection-check-script.js
+++ dist/connection-check-script.js
@@ -1,67 +1,59 @@
+// ----------------- Functions ------------------
 
-const elements = document.querySelectorAll('[id*="section-directions-trip-"]');
+function waitForElementToBeDefined(elementSelector, callback) {
+    const interval = setInterval(function() {
+        const targetElement = document.querySelector(elementSelector);
 
-// const iconUrl = chrome.runtime.getURL('/icons/4b74e5ea94181fde37324c605c8aa453--smiley-faces.jpg');
+        if (targetElement !== null && typeof targetElement !== 'undefined') {
+            // The element is defined. You can now execute your desired action.
+            clearInterval(interval); // Stop the interval.
+            callback(targetElement); // Execute the callback with the target element.
+        }
+    }, 100); // Check every 100 milliseconds for the element.
+}
 
-elements.forEach(element => {
-    // const icon = document.createElement('img');
-    // icon.src = iconUrl;
-    // icon.style.width = '100px';
-    // icon.style.height = '100px';
-    // icon.style.top = '0';
-    // icon.style.right = '0';
-    // icon.style.zIndex = '9999'; // Ensures the icon is on top of other content
-    // element.style.position = 'relative'; // Ensure the element's position is relative
 
-    // Append the icon to the element
-    // element.appendChild(icon);
-    const h3Element = document.createElement('h3');
-    h3Element.textContent = 'Hallo'; // Set the text content for the <h3> element
 
-    element.appendChild(h3Element);
+// ----------------- script loaded in website ------------------
+
+waitForElementToBeDefined('#ReiseloesungList > div.loading-indicator > div.reiseloesung-list-page__wrapper > div:nth-child(2) > ul > li', function(element) {
+
+    li_elements = document.querySelectorAll('#ReiseloesungList > div.loading-indicator > div.reiseloesung-list-page__wrapper > div:nth-child(2) > ul > li')
+
+    // ----------------- get origin & destination ------------------
+    li_element = li_elements[0]
+    start = document.querySelectorAll('span.test-reise-beschreibung-start-value');
+    const start_location = document.querySelectorAll('span.test-reise-beschreibung-start-value')[1].textContent;
+    const end_location = document.querySelectorAll('span.test-reise-beschreibung-ziel-value')[1].textContent;
+    console.log('START: ', start_location, 'END: ', end_location);
+
+
+    // ----------------- insert Buttons to results -----------------
+    li_elements.forEach(element => {
+        reiseplan_elements = element.querySelectorAll('[class*="reiseplan__infos"]')
+        const button = document.createElement('button');
+        button.textContent = 'Check network coverage'
+        button.style.backgroundColor = 'rgb(236, 0, 21)';          // Set the background color to red
+        button.style.color = 'white';                 // Set the text color to white
+        button.style.fontWeight = 'bold';             // Set the font weight to bold
+        button.style.borderRadius = '4px';            // Make the corners rounded
+        button.style.padding = '7px 20px';            // Adjust padding for appearance
+        button.style.height = '40px';
+
+        // TBD add image as button
+        // const iconImg = document.createElement('img');
+        // iconImg.src = chrome.runtime.getURL('signal-solid.svg');
+        // button.appendChild(iconImg);
+
+        // ----------------- on button click -----------------
+        button.addEventListener('click', function() {
+            // window.open('https://stable-meetings.nightf0rc3.de/demo', '_blank');
+            window.open(`https://stable-meetings.nightf0rc3.de/getGeo?origin=${start_location}&destination=${end_location}`, '_blank');
+        });
+        reiseplan_elements.forEach(element =>{
+            console.log('reiseplan element: ', element)
+            element.appendChild(button)
+        })
+    })
+    // --------------------------- END ---------------------------
 });
-
-
-
-// const icon_good = document.createElement("img_good");
-// const icon_ok = document.createElement("img_ok");
-// const icon_bad = document.createElement("img_bad");
-// const icon_good.src = chrome.extension.getURL("icon.png"); // Replace "icon.png" with the actual icon path
-// const icon_ok.src = chrome.extension.getURL("icon.png"); // Replace "icon.png" with the actual icon path
-// const icon_bad.src = chrome.extension.getURL("icon.png"); // Replace "icon.png" with the actual icon path
-// var icon;
-//
-// const requestData = {
-//     origin: 'Hello',
-//     destination: 'World',
-//     granularity: '1'
-// };
-//
-// // fetch route for each entry
-// // get entries
-// const fetchOptions = {
-//     method: 'GET',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(requestData),
-// };
-//
-// fetch("https://www.mywebsite.com/my-api", fetchOptions) //TODO add url
-//     .then(response => response.json())
-//     .then(data => {
-//         if (data === "good") {
-//            icon = icon_good
-//         }
-//         else if(data == "ok"){
-//             icon = icon_ok
-//         }
-//         else if(data == "bad"){
-//             icon = icon_bad
-//         }
-//         else
-//             icon = null
-//     });
-//
-// // Append the icon to the target field
-// targetField.appendChild(icon);
